a510b49ebf46b109e1da647ab0f263e539cf37fa
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-03-14 16:28:25 +0200

    added security initializer & config files

---
+++ spring-security-demo/src/main/java/com/stpunk47/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.stpunk47.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+extends AbstractSecurityWebApplicationInitializer {
+
+}
