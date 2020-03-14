a48d789970b535d5d92a408a6e9fadcdeec027c4
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-03-14 18:30:19 +0200

    in DemoConfig overrided method for configure(HttpSecurity)

--- spring-security-demo/src/main/java/com/stpunk47/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/stpunk47/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.stpunk47.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -21,6 +22,19 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 		.withUser(users.username("manager").password("test111").roles("MANAGER"))
 		.withUser(users.username("admin").password("admin").roles("ADMIN"));
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+				.and()
+				.formLogin()
+					.loginPage("/showMyLoginPage")
+					.loginProcessingUrl("/authenticateTheUser")
+					.permitAll();
+	}
+	
+	
 	
 	
 
