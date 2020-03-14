8df0da48139e2cd30684b7aa6673c86088809a3e
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-03-14 16:59:04 +0200

    updated SecurityConfig & added test users with pass in-memory

--- spring-security-demo/src/main/java/com/stpunk47/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/stpunk47/springsecurity/demo/config/DemoSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
@@ -11,8 +13,13 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(users.username("user").password("test111").roles("USER"))
+		.withUser(users.username("manager").password("test111").roles("MANAGER"))
+		.withUser(users.username("admin").password("admin").roles("ADMIN"));
 	}
 	
 	
--- spring-security-demo/src/main/java/com/stpunk47/springsecurity/demo/config/SecurityWebApplicationInitializer.java
+++ spring-security-demo/src/main/java/com/stpunk47/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -3,6 +3,10 @@ package com.stpunk47.springsecurity.demo.config;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityWebApplicationInitializer 
-extends AbstractSecurityWebApplicationInitializer {
+             extends AbstractSecurityWebApplicationInitializer {
+	
+	
+	
+	
 
 }
