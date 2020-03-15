bc2e822fba27b2742dba5c3b72398a9a062e0121
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-03-15 21:03:47 +0200

    defined DataSource

--- spring-security-demo/src/main/java/com/stpunk47/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo/src/main/java/com/stpunk47/springsecurity/demo/config/DemoAppConfig.java
@@ -1,8 +1,15 @@
 package com.stpunk47.springsecurity.demo.config;
 
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,7 +17,18 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.stpunk47.springsecurity")
+@PropertySource("classpath:persistance-mysql.properties")
 public class DemoAppConfig {
+	
+	//set up a variable to hold the properties
+	
+	@Autowired
+	private Environment env;
+	
+	//set up a logger for diagnostics
+	
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
 
 	//define a bean for ViewResolver
 	
@@ -24,4 +42,26 @@ public class DemoAppConfig {
 				
 		return viewResolver;
 	}
+	
+	//define a bean for security datasource
+	
+	@Bean
+	public DataSource securityDataSource() {
+		
+		//create connection pool
+		
+		//set JDBC driver class
+		
+		//log the connection props
+		
+		//set database connection props
+		
+		//set connection pool props
+		
+		
+		
+		return null;
+	}
+	
+	
 }
